31d8572a275a59ee5b0e2babf1fadd4cf47da7b3
florentpoujol <florentpoujol@fastmail.com>
2017-07-27 15:32:03 +0200

    add day 2015.4

---
+++ 2015/4.php
@@ -0,0 +1,26 @@
+<?php
+
+$input = "bgvyzdsv";
+$searchedId = 0;
+$searchedId2 = 0;
+$i = 0;
+
+while ($searchedId === 0 || $searchedId2 === 0) {
+    $i++;
+
+    $str = $input.$i;
+
+    $hash = md5($str);
+
+    if (substr($hash, 0, 5) === "00000") {
+        $searchedId = $i;
+    }
+
+    if (substr($hash, 0, 6) === "000000") {
+        $searchedId2 = $i;
+    }
+}
+
+
+echo "day 4.1: $searchedId <br>";
+echo "day 4.2: $searchedId2 <br>";
