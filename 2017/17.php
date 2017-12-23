c4f5d2f35d4d07d7f3868c8845d6fe09a19ff1ba
florentpoujol <florentpoujol@fastmail.com>
2017-12-23 17:30:39 +0100

    Finish day 2017.17 part 2

--- 2017/17.php
+++ 2017/17.php
@@ -2,7 +2,7 @@
 // http://adventofcode.com/2017/day/17
 
 $steps = 348;
-// $steps = 3; // test
+//$steps = 3; // test
 
 $buffer = [0];
 $bufferLength = 1;
@@ -10,47 +10,58 @@ $id = 0;
 
 for ($value = 1; $value <= 2017; $value++) {
     $stepsToEnd = $bufferLength - $id - 1;
-    $_steps = $steps;
+    $stepsCopy = $steps;
 
     if ($steps > $stepsToEnd) {
         $id = 0;
-        $_steps = $steps - ($stepsToEnd + 1); // remaining steps, from the beginning of the buffer
-        if ($_steps >= $bufferLength) {
-            $_steps -= $bufferLength * (int)($_steps / $bufferLength);
+        $stepsCopy = $steps - ($stepsToEnd + 1); // remaining steps, from the beginning of the buffer
+        if ($stepsCopy >= $bufferLength) {
+            $stepsCopy -= $bufferLength * (int)($stepsCopy / $bufferLength);
         }
     }
-    $id += $_steps + 1;
+    $id += $stepsCopy + 1;
 
     array_splice($buffer, $id, 0, $value);
     $bufferLength++;
 }
 
 $value = $buffer[$id + 1];
-echo "Day 17.1: $value";
+echo "Day 17.1: $value\n";
 
 
 // day 2
 
-// on my computer, it takes forever to compute the buffer up to a single million elements
-// I don't know to speed-up the algorithm
+// on my computer, it takes a good 2 minutes to compute only 100000 elements
+// so computing 50.000.000 elements would take a whole week with the same algo
 
-/*for ($value = 2018; $value <= 1E6; $value++) {
+$buffer = [0];
+$bufferLength = 1;
+$id = 0;
+
+$zeroId = 0;
+$idAfterZero = 1;
+$valueAfterZero = 1;
+
+for ($value = 1; $value <= 5E7; $value++) {
     $stepsToEnd = $bufferLength - $id - 1;
-    $_steps = $steps;
+    $stepsCopy = $steps;
 
     if ($steps > $stepsToEnd) {
         $id = 0;
-        $_steps = $steps - ($stepsToEnd + 1); // remaining steps, from the beginning of the buffer
-        if ($_steps >= $bufferLength) {
-            $_steps -= $bufferLength * (int)($_steps / $bufferLength);
+        $stepsCopy = $steps - ($stepsToEnd + 1); // remaining steps, from the beginning of the buffer
+        if ($stepsCopy >= $bufferLength) {
+            $stepsCopy -= $bufferLength * (int)($stepsCopy / $bufferLength);
         }
     }
-    $id += $_steps + 1;
-
-    array_splice($buffer, $id, 0, $value);
+    $id += $stepsCopy + 1;
     $bufferLength++;
+
+    if ($id === $zeroId) {
+        $zeroId++;
+        $idAfterZero++;
+    } elseif ($id === $idAfterZero) {
+        $valueAfterZero = $value;
+    }
 }
 
-$_id = array_search(0, $buffer);
-$value = $buffer[$_id + 1];
-echo "Day 17.2: $value";*/
+echo "Day 17.2: $valueAfterZero\n";
