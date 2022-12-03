dc538147161cf3a5dc55c171118402129f2d346e
Florent Poujol <florent.poujol@freshmile.com>
2022-12-03 21:41:38 +0100

    add day 2022.01 in PHP

---
+++ php/2022/01.php
@@ -0,0 +1,41 @@
+<?php
+
+$data = explode(PHP_EOL, file_get_contents('01_input.txt'));
+
+$maxCalories = 0;
+
+$caloriesForOneElf = 0;
+foreach ($data as $datum) {
+    if ($datum === '') {
+        $maxCalories = max($maxCalories, $caloriesForOneElf);
+        $caloriesForOneElf = 0;
+
+        continue;
+    }
+
+    $caloriesForOneElf += (int) $datum;
+}
+
+echo "01.1 : max calories $maxCalories" . PHP_EOL; // 70698
+
+// --------------------------------------------------
+
+$caloriesPerElf = [];
+
+$caloriesForOneElf = 0;
+foreach ($data as $datum) {
+    if ($datum === '') {
+        $caloriesPerElf[] = $caloriesForOneElf;
+        $caloriesForOneElf = 0;
+
+        continue;
+    }
+
+    $caloriesForOneElf += (int) $datum;
+}
+
+rsort($caloriesPerElf);
+
+$sum = array_sum(array_slice($caloriesPerElf, 0, 3));
+
+echo "01.2 : calories for the top three elves $sum" . PHP_EOL; // 206643
