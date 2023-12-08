67a6bcdaf8d2ba60bc4a89a0338016bd4370acc0
Florent Poujol <florent.poujol@freshmile.com>
2023-12-08 14:55:01 +0100

    finish 2023.05 part 2

--- php/2023/05.php
+++ php/2023/05.php
@@ -8,13 +8,15 @@ $handle = fopen('input/05.txt', 'r');
 
 final class Range {
     private readonly int $sourceEnd;
+    private readonly int $destinationEnd;
 
     public function __construct(
         private readonly int $destinationStart,
         public readonly int $sourceStart,
         public readonly int $length,
     ) {
-        $this->sourceEnd = $this->sourceStart + $this->length;
+        $this->sourceEnd = $this->sourceStart + ($this->length - 1);
+        $this->destinationEnd = $this->destinationStart + ($this->length - 1);
     }
 
     public function isSourceInRange(int $source): bool
@@ -27,6 +29,18 @@ final class Range {
         $offset = $source - $this->sourceStart;
 
         return $this->destinationStart + $offset;
+    }
+
+    public function isDestinationInRange(int $destination): bool
+    {
+        return $destination >= $this->destinationStart && $destination <= $this->destinationEnd;
+    }
+
+    public function getSource(int $destination): int
+    {
+        $offset = $destination - $this->destinationStart;
+
+        return $this->sourceStart + $offset;
     }
 }
 
@@ -129,49 +143,99 @@ printDay("05.1: $smallestLocation"); // 0.55 ms
 rewind($handle);
 startTimer();
 
-$smallestLocation = PHP_INT_MAX;
-
-$seedRanges = array_chunk($seeds, 2);
-
-/** @var array<string, array<int, int>> $destinationCachePerMapName */
-$destinationCachePerMapName = [
-    // keys are map names
-    // values are array<int, int>, key is the source, value is the destination
-];
-
-foreach ($seedRanges as $i => $seedRange) {
-    $minSeed = $seedRange[0];
-    $length = $seedRange[1];
-    display("starting for seed range $i", $seedRange[0], $seedRange[1]);
-
-    $j = 0;
-    for (; $length >= 0; --$length) {
-        $destination = $minSeed + $length;
-        foreach ($maps as $mapName => $map) {
-            if ($destination < $map['source_min'] || $destination > $map['source_max']) {
-                continue;
-                // this "optimisation" doesn't work at all because the function findDestinationInRanges() still gets called as many times as before...
-            }
-
-            $destination = findDestinationInRanges($map['ranges'], $destination);
-        }
-
-        if ($destination < $smallestLocation) {
-            $smallestLocation = $destination;
-        }
-
-        $j++;
-        if ($j % 10_000 === 0) {
-            echo '.';
-        }
-        if ($j % 500_000 === 0) {
-            echo "length remaining: $length" . PHP_EOL;
-            exit;
-        }
-    }
-}
-
+// $smallestLocation = PHP_INT_MAX;
+// $seedRanges = array_chunk($seeds, 2);
+//
+// foreach ($seedRanges as $i => $seedRange) {
+//     $minSeed = $seedRange[0];
+//     $length = $seedRange[1];
+//     display("starting for seed range $i", $seedRange[0], $seedRange[1]);
+//
+//     $j = 0;
+//     for (; $length >= 0; --$length) {
+//         $destination = $minSeed + $length;
+//         foreach ($maps as $mapName => $map) {
+//             if ($destination < $map['source_min'] || $destination > $map['source_max']) {
+//                 continue;
+//                 // this "optimisation" doesn't work at all because the function findDestinationInRanges() still gets called as many times as before...
+//             }
+//
+//             $destination = findDestinationInRanges($map['ranges'], $destination);
+//         }
+//
+//         if ($destination < $smallestLocation) {
+//             $smallestLocation = $destination;
+//         }
+//
+//         $j++;
+//         if ($j % 10_000 === 0) {
+//             echo '.';
+//         }
+//         if ($j % 500_000 === 0) {
+//             echo "length remaining: $length" . PHP_EOL;
+//             exit;
+//         }
+//     }
+// }
 // Note : /!\ this doesn't works ! /!\
 // is infinitely too slow, like 10 seconds to check a million seeds, and yet there is a total of a few billions seeds...
 
-printDay("05.1: $smallestLocation"); //
+
+// All of this commented code above may be right but is way too slow.
+// Instead of finding the location based on the seeds, we will try every location, starting from 0, until we find a matching seed.
+// (I got the idea from Reddit)
+
+$location = 0;
+$seed = 0;
+
+$reversedMaps = array_reverse($maps, true);
+
+/**
+ * @param array<Range> $map
+ */
+function findSourceInRanges(array $map, int $destination): int
+{
+    foreach ($map as $range) {
+        if ($range->isDestinationInRange($destination)) {
+            return $range->getSource($destination);
+        }
+    }
+
+    return $destination;
+}
+
+$seedRanges = array_chunk($seeds, 2);
+
+foreach ($seedRanges as & $seedRange) { // /!\ REFERENCE
+    $seedRange[2] = $seedRange[0] + ($seedRange[1] - 1);
+}
+unset($seedRange);
+
+do {
+    $location++;
+
+    $destination = $location;
+    foreach ($reversedMaps as $mapName => $reversedMap) {
+        $source = $destination;
+        $destination = findSourceInRanges($reversedMap['ranges'], $destination);
+        // display($mapName, $destination, $source);
+    }
+    // $destination is now a seed
+
+    foreach ($seedRanges as $seedRange) {
+        if ($destination >= $seedRange[0] && $destination <= $seedRange[2]) { // seed is within range
+            $seed = $destination;
+            // display($location, $seed);
+            break(2);
+        }
+    }
+
+    if ($location % 10_000 === 0) {
+        echo '.';
+    }
+    if ($location % 1_000_000 === 0) {
+        echo PHP_EOL;
+    }
+} while (true);
+
+printDay("05.1: $location | seed ($seed)"); // 669 s / 11 m (location 37806486)
